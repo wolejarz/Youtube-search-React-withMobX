af5f4b29f055f4450d0eb3e898458e636a484306
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-03-14 14:03:40 +0100

    await added with async recursive call

--- src/stores/videoStore.js
+++ src/stores/videoStore.js
@@ -30,39 +30,12 @@ class videoStore {
     const filteredVideos = responseInJson.items.filter(current =>
       this.hiddenOrWatchedVideos.indexOf(current.id.videoId) === -1 ? true : false
     );
-    console.log(howManyINeed, pageToken);
-    console.log(filteredVideos.length);
-    console.log(filteredVideos);
-    console.log('next .........');
-    if (filteredVideos.length < howManyINeed)
-      filteredVideos.concat(
-        this.getVideosFromChannel(channel, howManyINeed - filteredVideos.length, responseInJson.nextPageToken)
-      );
-    return filteredVideos;
+    return filteredVideos.length < howManyINeed
+      ? filteredVideos.concat(
+          await this.getVideosFromChannel(channel, howManyINeed - filteredVideos.length, responseInJson.nextPageToken)
+        )
+      : filteredVideos;
   };
-
-  //gets  N (MAX_VIDEOS) videos from given channel - only non-watched or non-hidden videos
-  // getVideosFromChannel = async channel => {
-  //   let videosFromChannel = null;
-  //   let pageToken = "";
-  //   try {
-  //     while (videosFromChannel === null || videosFromChannel.length < MAX_VIDEOS) {
-  //       const response = await fetch(
-  //         `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel.channelId}
-  //         &maxResults=${MAX_VIDEOS}&order=date&pageToken=${pageToken}&type=video&key=${APIKey}`  );
-  //       const responseInJson = await response.json();
-  //       pageToken = responseInJson.nextPageToken;
-  //       const filteredVideosFromResponse = responseInJson.items.filter(current =>
-  //         this.hiddenOrWatchedVideos.indexOf(current.id.videoId) === -1 ? true : false );
-  //       videosFromChannel = [...videosFromChannel||[],...filteredVideosFromResponse||[]];
-  //     }
-  //     return videosFromChannel.map(current => ({
-  //       id: current.id.videoId,
-  //       description: current.snippet.description,
-  //       publishTime: current.snippet.publishTime,
-  //     }));
-  //   } catch (error) { console.log('API Error'); }
-  // }
 
   handleGetVideos = async () => {
     const allChannelsUnselected = !ChannelStore.channels.reduce((total, current) => total || current.selected, false);
